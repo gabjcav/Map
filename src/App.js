e529f24d5c3823a8b7cba21dea8556ef6c8d2ae1
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-01-31 00:52:34 +0100

    connected to cosmicjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Cosmic from 'cosmicjs';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Mapbox from 'mapbox-gl';
+
+let map = null; 
 
 function App() {
+  const mapElement = useRef(null)
+  const [pageData, setPageData] = useState(null);
+
+  useEffect(() => {
+    const client = new Cosmic()
+    const bucket = client.bucket({
+      slug: process.env.BUCKET_SLUG,
+      read_key: process.env.READ_KEY
+    });
+
+    bucket.getObject({
+      slug: 'my-top-5-places-to-travel',
+      props: 'slug,title,content,metadata'
+    })
+    .then(data => {
+      setPageData(data.object)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, []);
+
+  function renderSkeleton() {
+    return (
+      <p>Loading page....</p>
+    );
+  }
+
+  function renderPage() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <div dangerouslySetInnerHTML={{__html: pageData.content}} />
+      </div>
+    )
+  }
+
   return (
     <>
-      <h1>Hallo verden!</h1>
-      <p>
-        Jeg er en veldig enkel React-app som du kan endre og leke med som du vil.
-      </p>
+      {(pageData === null) ? renderSkeleton() : renderPage()}
     </>
   )
 };
